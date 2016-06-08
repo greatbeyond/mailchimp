fdae86f50b6b66223eab61fa0d365ddfc97e054b
David Högborg <david.hogborg@greatbeyond.se>
2016-06-08 18:06:16 +0200

    Campaign structure and actions

--- campaign.go
+++ campaign.go
@@ -12,26 +12,77 @@ import (
 	"github.com/antonholmquist/jason"
 )
 
-// CAMPAIGNS_URL is the url endpoint for campaign on mailchimp v3
-const CAMPAIGNS_URL = "/campaigns"
+const (
+	// CampaignsURL is the url endpoint for campaign on mailchimp v3
+	CampaignsURL = "/campaigns"
+	// CampaignContentURL is the url endpoint for campaign content on mailchimp v3
+	CampaignContentURL = "/content"
+
+	CampaignActionCancel     = "/actions/cancel-send" //	Cancel a campaign
+	CampaignActionPause      = "/actions/pause"       //	Pause an RSS-Driven campaign
+	CampaignActionResume     = "/actions/resume"      //	Resume an RSS-Driven campaign
+	CampaignActionSchedule   = "/actions/schedule"    //	Schedule a campaign
+	CampaignActionSend       = "/actions/send"        //	Send a campaign
+	CampaignActionTest       = "/actions/test"        //	Send a test email
+	CampaignActionUnschedule = "/actions/unschedule"  //	Unschedule a campaign
+	// CampaignActionReplicate  = "/actions/replicate"   //	Replicate a campaign ( not implemented )
+)
 
 // Campaign defines a campaign on mailchimp
 type Campaign struct {
-	ID          string `json:"id"`
-	Type        string `json:"type"`
-	CreateTime  string `json:"create_time"`
-	ArchiveURL  string `json:"archive_url"`
-	Status      string `json:"status"`
-	EmailsSent  int    `json:"emails_sent"`
-	SendTime    string `json:"send_time"`
-	ContentType string `json:"content_type"`
-	Recipients  string `json:"recipients"`
-	ListID      string `json:"list_id"`
-	SegmentText string `json:"segment_text"`
+	// A string that uniquely identifies this campaign.
+	ID string `                              json:"id"`
 
-	Settings       *CampaignSettings       `json:"settings"`
-	Tracking       *CampaignTracking       `json:"tracking"`
-	DeliveryStatus *CampaignDeliveryStatus `json:"delivery_status"`
+	// There are four types of campaigns you can create in MailChimp. A/B Split campaigns have been deprecated and variate campaigns should be used instead.
+	Type string `                            json:"type"`
+
+	// The date and time the campaign was created.
+	CreateTime string `                      json:"create_time"`
+
+	// The link to the campaign’s archive version.
+	ArchiveURL string `                      json:"archive_url"`
+
+	// The original link to the campaign’s archive version.
+	LongArchiveURL string `                  json:"long_archive_url"`
+
+	// The current status of the campaign.
+	Status string `                          json:"status"`
+
+	// The total number of emails sent for this campaign.
+	EmailsSent int `                         json:"emails_sent"`
+
+	// The date and time a campaign was sent.
+	SendTime string `                        json:"send_time"`
+
+	// How the campaign’s content is put together (‘template’, ‘drag_and_drop’, ‘html’, ‘url’).
+	ContentType string `                     json:"content_type"`
+
+	// List settings for the campaign.
+	Recipients *CampaignRecipient `          json:"recipients"`
+
+	// The settings for your campaign, including subject, from name, reply-to address, and more.
+	Settings *CampaignSettings `             json:"settings"`
+
+	// The settings specific to A/B test campaigns.
+	VariateSettings interface{} `            json:"variate_settings"`
+
+	// The tracking options for a campaign.
+	Tracking *CampaignTracking `             json:"tracking"`
+
+	// RSS options for a campaign.
+	RSSOpts interface{} `                    json:"rss_opts"`
+
+	// A/B Testing options for a campaign.
+	ABSplitOpts interface{} `                json:"ab_split_opts"`
+
+	// The preview for the campaign, rendered by social networks like Facebook and Twitter. Learn more.
+	SocialCard interface{} `                 json:"social_card"`
+
+	// For sent campaigns, a summary of opens, clicks, and unsubscribes.
+	ReportSummary interface{} `              json:"report_summary"`
+
+	// Updates on campaigns in the process of sending.
+	DeliveryStatus *CampaignDeliveryStatus ` json:"delivery_status"`
 
 	// Internal
 	client *Client
@@ -39,33 +90,89 @@ type Campaign struct {
 
 // CampaignSettings defines settings for a campaign
 type CampaignSettings struct {
-	SubjectLine     string `json:"subject_line"`
-	Title           string `json:"title"`
-	FromName        string `json:"from_name"`
-	ReplyTo         string `json:"reply_to"`
-	UseConversation bool   `json:"use_conversation"`
-	ToName          string `json:"to_name"`
-	FolderID        string `json:"folder_id"`
-	Authenticate    bool   `json:"authenticate"`
-	AutoFooter      bool   `json:"auto_footer"`
-	InlineCSS       bool   `json:"inline_css"`
-	AutoTweet       bool   `json:"auto_tweet"`
-	FbComments      bool   `json:"fb_comments"`
-	Timewarp        bool   `json:"timewarp"`
-	TemplateID      int    `json:"template_id"`
-	DragAndDrop     bool   `json:"drag_and_drop"`
+	// The subject line for the campaign.
+	SubjectLine string `         json:"subject_line"`
+
+	// The title of the campaign.
+	Title string `               json:"title"`
+
+	// The ‘from’ name on the campaign (not an email address).
+	FromName string `            json:"from_name"`
+
+	// The reply-to email address for the campaign.
+	ReplyTo string `             json:"reply_to"`
+
+	// Use MailChimp Conversation feature to manage out-of-office replies.
+	UseConversation bool `       json:"use_conversation"`
+
+	// The campaign’s custom ‘To’ name. Typically the first name merge field.
+	ToName string `              json:"to_name"`
+
+	// If the campaign is listed in a folder, the id for that folder.
+	FolderID string `            json:"folder_id"`
+
+	// Whether MailChimp authenticated the campaign. Defaults to true.
+	Authenticate bool `          json:"authenticate"`
+
+	// Automatically append MailChimp’s default footer to the campaign.
+	AutoFooter bool `            json:"auto_footer"`
+
+	// Automatically inline the CSS included with the campaign content.
+	InlineCSS bool `             json:"inline_css"`
+
+	// Automatically tweet a link to the campaign archive page when the campaign is sent.
+	AutoTweet bool `             json:"auto_tweet"`
+
+	// An array of Facebook page ids to auto-post to.
+	AutoFbPost []string `        json:"auto_fb_post"`
+
+	// Allows Facebook comments on the campaign (also force-enables the Campaign Archive toolbar). Defaults to true.
+	FbComments bool `            json:"fb_comments"`
+
+	// Send this campaign using Timewarp.
+	Timewarp bool `              json:"timewarp"`
+
+	// The id for the template used in this campaign.
+	TemplateID int `             json:"template_id"`
+
+	// Whether the campaign uses the drag-and-drop editor.
+	DragAndDrop bool `           json:"drag_and_drop"`
 }
 
+// CampaignTracking settings
 type CampaignTracking struct {
-	Opens           bool   `json:"opens"`
-	HTMLClicks      bool   `json:"html_clicks"`
-	TextClicks      bool   `json:"text_clicks"`
-	GoalTracking    bool   `json:"goal_tracking"`
-	Ecomm360        bool   `json:"ecomm360"`
-	GoogleAnalytics string `json:"google_analytics"`
-	Clicktale       string `json:"clicktale"`
+	// Whether to track opens. Defaults to true. Cannot be set to false for variate campaigns.
+	Opens bool `                 json:"opens"`
+
+	// Whether to track clicks in the HTML version of the campaign. Defaults to true. Cannot be set to false for variate campaigns.
+	HTMLClicks bool `            json:"html_clicks"`
+
+	// Whether to track clicks in the plain-text version of the campaign. Defaults to true. Cannot be set to false for variate campaigns.
+	TextClicks bool `            json:"text_clicks"`
+
+	// Whether to enable Goal tracking.
+	GoalTracking bool `          json:"goal_tracking"`
+
+	// Whether to enable E-commerce tracking.
+	Ecomm360 bool `              json:"ecomm360"`
+
+	// The custom slug for Google Analytics tracking (max of 50 bytes).
+	GoogleAnalytics string `     json:"google_analytics"`
+
+	// The custom slug for ClickTale tracking (max of 50 bytes).
+	Clicktale string `           json:"clicktale"`
+
+	// Salesforce tracking options for a campaign. Must be using MailChimp’s built-in Salesforce integration.
+	Salesforce interface{} `     json:"salesforce"`
+
+	// Highrise tracking options for a campaign. Must be using MailChimp’s built-in Highrise integration.
+	Highrise interface{} `       json:"highrise"`
+
+	// Capsule tracking options for a campaign. Must be using MailChimp’s built-in Capsule integration.
+	Capsule interface{} `        json:"capsule"`
 }
 
+// CampaignDeliveryStatus updates on campaigns in the process of sending.
 type CampaignDeliveryStatus struct {
 	Enabled bool `json:"enabled"`
 }
@@ -118,61 +225,67 @@ type UpdateCampaign CreateCampaign
 // CampaignRecipient defines default fields for a reciptient
 type CampaignRecipient struct {
 	// The unique list id.
-	ListID string `json:"list_id"`
+	ListID string `              json:"list_id"`
+
+	// The name of the list.
+	ListName string `            json:"list_name"`
 
 	// A string marked-up with HTML explaining the
 	// segment used for the campaign in plain English.
-	SegmentText string `json:"segment_text"`
+	SegmentText string `         json:"segment_text"`
+
+	// Count of the recipients on the associated list. Formatted as an integer.
+	RecipientCount int `         json:"recipient_count"`
 
 	// An object representing all segmentation options.
-	SegmentOpts interface{} `json:"segment_opts"`
+	SegmentOpts interface{} `    json:"segment_opts"`
 }
 
 // CampaignCreateSettings Required fields for campaing creation settings
 type CampaignCreateSettings struct {
 	// The subject line for the campaign.
-	SubjectLine string `json:"subject_line"`
+	SubjectLine string `         json:"subject_line"`
 
 	// The title of the campaign.
-	Title string `json:"title"`
+	Title string `               json:"title"`
 
 	// The ‘from’ name on the campaign (not an email address).
-	FromName string `json:"from_name"`
+	FromName string `            json:"from_name"`
 
 	// The reply-to email address for the campaign.
-	ReplyTo string `json:"reply_to"`
+	ReplyTo string `             json:"reply_to"`
 
 	// Use MailChimp Conversation feature to manage out-of-office replies.
-	UseConversation bool `json:"use_conversation"`
+	UseConversation bool `       json:"use_conversation"`
 
 	// The campaign’s custom ‘To’ name. Typically the first name merge field.
-	ToName string `json:"to_name"`
+	ToName string `              json:"to_name"`
 
 	// If the campaign is listed in a folder, the id for that folder.
-	FolderID string `json:"folder_id"`
+	FolderID string `            json:"folder_id"`
 
 	// Whether MailChimp authenticated the campaign. Defaults to true.
-	Authenticate bool `json:"authenticate"`
+	Authenticate bool `          json:"authenticate"`
 
 	// Automatically append MailChimp’s default footer to the campaign.
-	AutoFooter bool `json:"auto_footer"`
+	AutoFooter bool `            json:"auto_footer"`
 
 	// Automatically inline the CSS included with the campaign content.
-	InlineCSS bool `json:"inline_css"`
+	InlineCSS bool `             json:"inline_css"`
 
 	// Automatically tweet a link to the campaign archive page when the campaign is sent.
-	AutoTweet bool `json:"auto_tweet"`
+	AutoTweet bool `             json:"auto_tweet"`
 
 	// An array of Facebook page ids to auto-post to.
-	AutoFbPost []string `json:"auto_fb_post"`
+	AutoFbPost []string `        json:"auto_fb_post"`
 
 	// Allows Facebook comments on the campaign (also force-enables the Campaign Archive toolbar). Defaults to true.
-	FbComments bool `json:"fb_comments"`
+	FbComments bool `            json:"fb_comments"`
 }
 
 // NewCampaign creates a new campaign via mailchimp api v3
 func (c *Client) NewCampaign(data *CreateCampaign) (*Campaign, error) {
-	response, err := c.post(CAMPAIGNS_URL, nil, data)
+	response, err := c.post(CampaignsURL, nil, data)
 
 	var campaign *Campaign
 	err = json.Unmarshal(response, &campaign)
@@ -188,9 +301,12 @@ func (c *Client) NewCampaign(data *CreateCampaign) (*Campaign, error) {
 	return campaign, nil
 }
 
+// -----------------------------------------------------------------
+// Retrive and update
+
 // GetCampaigns retrives all campaigns from mailchimp
 func (c *Client) GetCampaigns() ([]*Campaign, error) {
-	response, err := c.get(CAMPAIGNS_URL, nil)
+	response, err := c.get(CampaignsURL, nil)
 
 	v, err := jason.NewObjectFromBytes(response)
 	if err != nil {
@@ -200,7 +316,7 @@ func (c *Client) GetCampaigns() ([]*Campaign, error) {
 		return nil, err
 	}
 
-	_lists, err := v.GetValue("campaigns")
+	_campaigns, err := v.GetValue("campaigns")
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err.Error(),
@@ -208,7 +324,7 @@ func (c *Client) GetCampaigns() ([]*Campaign, error) {
 		return nil, err
 	}
 
-	b, err := _lists.Marshal()
+	b, err := _campaigns.Marshal()
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err.Error(),
@@ -234,7 +350,7 @@ func (c *Client) GetCampaigns() ([]*Campaign, error) {
 
 // GetCampaign retrives a single campaign by id
 func (c *Client) GetCampaign(id string) (*Campaign, error) {
-	response, err := c.get(slashJoin(CAMPAIGNS_URL, id), nil)
+	response, err := c.get(slashJoin(CampaignsURL, id), nil)
 
 	var campaign *Campaign
 	err = json.Unmarshal(response, &campaign)
@@ -253,7 +369,7 @@ func (c *Client) GetCampaign(id string) (*Campaign, error) {
 
 // Update sets new values on a campaign via mailchimp api
 func (c *Campaign) Update(data *UpdateCampaign) (*Campaign, error) {
-	response, err := c.client.patch(slashJoin(CAMPAIGNS_URL, c.ID), nil, data)
+	response, err := c.client.patch(slashJoin(CampaignsURL, c.ID), nil, data)
 
 	var campaign *Campaign
 	err = json.Unmarshal(response, &campaign)
@@ -270,9 +386,12 @@ func (c *Campaign) Update(data *UpdateCampaign) (*Campaign, error) {
 	return campaign, nil
 }
 
-// Delete removes a campaign
-func (c *Campaign) Delete() error {
-	err := c.client.delete(slashJoin(CAMPAIGNS_URL, c.ID))
+// -----------------------------------------------------------------
+// Actions on campaign
+
+// Cancel a campaign
+func (c *Campaign) Cancel() error {
+	_, err := c.client.post(slashJoin(CampaignsURL, c.ID, CampaignActionCancel), nil, nil)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"ID":    c.ID,
@@ -281,4 +400,188 @@ func (c *Campaign) Delete() error {
 		return err
 	}
 	return nil
+}
+
+// Pause a campaign
+func (c *Campaign) Pause() error {
+	_, err := c.client.post(slashJoin(CampaignsURL, c.ID, CampaignActionPause), nil, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return err
+	}
+	return nil
+}
+
+// Resume a campaign
+func (c *Campaign) Resume() error {
+	_, err := c.client.post(slashJoin(CampaignsURL, c.ID, CampaignActionResume), nil, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return err
+	}
+	return nil
+}
+
+// Schedule a campaign
+func (c *Campaign) Schedule() error {
+	_, err := c.client.post(slashJoin(CampaignsURL, c.ID, CampaignActionSchedule), nil, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return err
+	}
+	return nil
+}
+
+// Send a campaign
+func (c *Campaign) Send() error {
+	_, err := c.client.post(slashJoin(CampaignsURL, c.ID, CampaignActionSend), nil, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return err
+	}
+	return nil
+}
+
+// Test a campaign
+func (c *Campaign) Test() error {
+	_, err := c.client.post(slashJoin(CampaignsURL, c.ID, CampaignActionTest), nil, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return err
+	}
+	return nil
+}
+
+// Unschedule a campaign
+func (c *Campaign) Unschedule() error {
+	_, err := c.client.post(slashJoin(CampaignsURL, c.ID, CampaignActionUnschedule), nil, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return err
+	}
+	return nil
+}
+
+// Delete removes a campaign
+func (c *Campaign) Delete() error {
+	err := c.client.delete(slashJoin(CampaignsURL, c.ID))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return err
+	}
+	return nil
+}
+
+// -----------------------------------------------------------------
+// Content manipulation on the campaign
+
+// CampaignContent can be retrived
+type CampaignContent struct {
+	// Content options for multivariate campaigns.
+	VariateContents struct {
+		// Label used to identify the content option.
+		ContentLabel string `     json:"content_label"`
+
+		// The plain-text portion of the campaign. If left unspecified, we’ll generate this automatically.
+		PlainText string `        json:"plain_text"`
+
+		// The raw HTML for the campaign.
+		HTML string `             json:"html"`
+	} `json:"variate_contents"`
+
+	// The plain-text portion of the campaign. If left unspecified, we’ll generate this automatically.
+	PlainText string `            json:"plain_text"`
+
+	// The raw HTML for the campaign.
+	HTML string `                 json:"html"`
+}
+
+type CampaignContentEdit struct {
+	// The plain-text portion of the campaign. If left unspecified, we’ll generate this automatically.
+	PlainText string `          json:"plain_text"`
+
+	// The raw HTML for the campaign.
+	HTML string `               json:"html"`
+
+	// When importing a campaign, the URL where the HTML lives.
+	URL string `                json:"url"`
+
+	// Use this template to generate the HTML content of the campaign
+	Template struct {
+		// The id of the template to use.
+		ID int `                json:"id"`
+
+		// Content for the sections of the template. Each key should be the unique mc:edit area name from the template.
+		Sections interface{} `  json:"sections"`
+	} `json:"template"`
+
+	// Available when uploading an archive to create campaign content. The archive should include all campaign content and images. Learn more.
+	Archive struct {
+		// he base64-encoded representation of the archive file.
+		ArchiveContent string `json:"archive_content"`
+
+		// The type of encoded file. Defaults to zip.
+		// Possible Values:
+		// 	zip tar.gz tar.bz2 tar tgz tbz
+		ArchiveType string `   json:"archive_type"`
+	} `json:"archive"`
+
+	// Content options for Multivariate Campaigns. Each content option must provide HTML content and may optionally provide plain text. For campaigns not testing content, only one object should be provided.
+	VariateContents []interface{} `json:"variate_contents"`
+}
+
+// GetContent retrives the content for a campaign
+func (c *Campaign) GetContent() (interface{}, error) {
+
+	response, err := c.client.get(slashJoin(CampaignsURL, c.ID, CampaignContentURL), nil)
+
+	var content *CampaignContent
+	err = json.Unmarshal(response, &content)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return nil, err
+	}
+
+	return content, nil
+}
+
+// SetContent updates the content for the campaign
+func (c *Campaign) SetContent(content *CampaignContentEdit) (*CampaignContent, error) {
+	response, err := c.client.put(slashJoin(CampaignsURL, c.ID, CampaignContentURL), nil, content)
+
+	var responseContent *CampaignContent
+	err = json.Unmarshal(response, &responseContent)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"ID":    c.ID,
+			"error": err.Error(),
+		}).Warn("response error", caller())
+		return nil, err
+	}
+
+	return responseContent, nil
 }
